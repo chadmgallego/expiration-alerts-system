066d64950d7472ed3ecd4f4470c32a5e8bd25d66
Chad Gallego <chadmgallego@gmail.com>
2026-04-16 11:31:30 +0900

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -20,4 +20,9 @@ CREATE TABLE DOCUMENTS (
     expiration_date DATE,
     last_alert_sent DATE,
     last_alert_type INTEGER  -- stores 30, 60, or 90 depending on last alert tier sent
+    expired BOOLEAN DEFAULT FALSE,  -- set to TRUE when expiration_date < CURRENT_DATE
 );
+
+-- NOTE: If adding these columns to an existing table, run:
+-- ALTER TABLE DOCUMENTS ADD COLUMN expired BOOLEAN DEFAULT FALSE;
+-- ALTER TABLE DOCUMENTS ADD CONSTRAINT unique_person_document UNIQUE (personal_id, document_type);
